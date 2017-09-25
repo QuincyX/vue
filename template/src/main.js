66bc696c290fea24849c44c4e143aef61fa08485
Quincy <jayever@163.com>
2017-09-25 17:11:08 +0800

    add global directives

--- template/src/main.js
+++ template/src/main.js
@@ -4,9 +4,13 @@ import router from './router'
 import store from './store'
 import API from '@/api'
 import * as filters from './common/filters'
+import * as directives from './common/directives'
 
 Object.keys(filters).forEach(key => {
   Vue.filter(key, filters[key])
+})
+Object.keys(directives).forEach(key => {
+  Vue.directive(key, directives[key])
 })
 
 Vue.prototype.$API = API

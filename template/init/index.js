85d16fbafb022990155840504257127e9999ebaf
Quincy <jayever@163.com>
2017-09-27 16:50:51 +0800

    u

--- template/init/index.js
+++ template/init/index.js
@@ -4,7 +4,21 @@ const router = express.Router()
 const initTool = require('./tool.js')
 
 router.post('/', (req, res) => {
-  initTool.initPages(req.body)
+  let thisModules = req.body.pages.map(o => {
+    if (o.module !== '/') {
+      return o.module
+    }
+  })
+  if (req.body.api) {
+    initTool.initApi(thisModules)
+  }
+  if (req.body.vuex) {
+    initTool.initVuex(thisModules)
+  }
+  if (req.body.router) {
+    initTool.initRouter()
+  }
+  initTool.initPages(req.body.pages)
   res.send('init pages success')
 })
 
--- template/init/tool.js
+++ template/init/tool.js
@@ -1,5 +1,5 @@
 var fs = require('fs'),
-  path = require('path');
+path = require('path');
 
 // 用户配置信息
 
@@ -19,63 +19,109 @@ let routerFile = './src/router/index.js'
 
 // 功能定义
 function createVueFileFromOrigin(fileName) {
-  let fileData = fs.readFileSync(originFile, 'utf-8').replace(replaceName, 'this is ' + fileName + ' page')
-  fs.writeFileSync(basePath + fileName + '.vue', fileData)
+let fileData = fs.readFileSync(originFile, 'utf-8').replace(replaceName, 'this is ' + fileName + ' page')
+fs.writeFileSync(basePath + fileName + '.vue', fileData)
 }
 
 function createApiModule(fileName) {
-  fs.writeFileSync('./src/api/' + fileName + '.js', fs.readFileSync('./src/api/module.js', 'utf-8').replace(/test/g, fileName))
+fs.writeFileSync('./src/api/' + fileName + '.js', fs.readFileSync('./src/api/module.js', 'utf-8').replace(/test/g, fileName))
 }
 
 function createStoreModule(fileName) {
-  fs.writeFileSync('./src/store/modules/' + fileName + '.js', fs.readFileSync('./src/store/modules/module.js', 'utf-8'))
+fs.writeFileSync('./src/store/modules/' + fileName + '.js', fs.readFileSync('./src/store/modules/module.js', 'utf-8'))
+}
+
+async function findFile(p) {
+let files = await fs.readdirSync(p)
+for (let i = 0; i < files.length; i++) {
+  if (files[i].split('.').length == 1) {
+    await findFile(p + '/' + files[i])
+  } else if (files[i].split('.')[1] === 'vue') {
+    let n = {}
+    if (p === basePath) {
+      n.name = files[i].split('.')[0]
+    } else {
+      n.name = p.slice(basePath.length + 1).replace(/\//g, "_") + '_' + files[i].split('.')[0]
+    }
+    n.link = p.slice(basePath.length) + '/' + files[i].split('.')[0]
+    n.path = "@/" + basePath.split('/').pop() + p.slice(basePath.length) + '/' + files[i].split('.')[0]
+    vueFileList.push(n)
+  }
+}
 }
 
 module.exports = {
-  initPages: para => {
-    para.forEach(o => {
-      if (o.module === '/') {
-        o.vueFile.forEach(v => {
-          createVueFileFromOrigin('/' + v.name)
-          console.log('create ' + v.name + ' success')
-        })
-      } else {
-        fs.mkdirSync(path.join(__dirname, '..', basePath, ('/' + o.module)))
-        o.vueFile.forEach(v => {
-          createVueFileFromOrigin('/' + o.module + '/' + v.name)
-          console.log('create ' + v.name + ' success')
-        })
-      }
-    })
-  },
-  initRouter: para => {
-    if (fs.existsSync(routerFile)) {
-      let bakFile = routerFile.replace(/index\.js/, 'index_bak.js')
-      fs.renameSync(routerFile, bakFile)
-    }
-  },
-  initApi: para => {
-    let apiFileAppend = '// 引入API模块\r\n'
-    let apiFileAppend2 = 'Object.assign(moduleAPI'
-    para.forEach(o => {
-      if (!fs.existsSync(`./src/api/${o}.js`)) {
-        createApiModule(o)
-        apiFileAppend += `import * as ${o}API from './${o}'\r\n`
-        apiFileAppend2 += `, ${o}API`
-      }
-    })
-    fs.writeFileSync('./src/api/index.js', fs.readFileSync('./src/api/index.js', 'utf-8').replace(/\/\/ 引入API模块/, apiFileAppend).replace(/Object\.assign\(moduleAPI/, apiFileAppend2))
-  },
-  initVuex: para => {
-    let storeFileAppend = '// 引入vuex模块\r\n'
-    let storeFileAppend2 = 'modules: {\r\n'
-    para.forEach(o => {
-      if (!fs.existsSync(`./src/store/modules/${o}.js`)) {
-        createStoreModule(o)
-        storeFileAppend += `import ${o}Module from './modules/${o}'\r\n`
-        storeFileAppend2 += `\t\t${o}Module,\r\n`
-      }
-    })
-    fs.writeFileSync('./src/store/index.js', fs.readFileSync('./src/store/index.js', 'utf-8').replace(/\/\/ 引入vuex模块/, storeFileAppend).replace(/modules: \{/, storeFileAppend2))
+initPages: para => {
+  if (!fs.existsSync(path.join(__dirname, '..', basePath))) {
+    fs.mkdirSync(path.join(__dirname, '..', basePath))
   }
+  para.forEach(o => {
+    if (o.module === '/') {
+      o.vueFile.forEach(v => {
+        createVueFileFromOrigin('/' + v.name)
+        console.log('create ' + v.name + ' success')
+      })
+    } else {
+      fs.mkdirSync(path.join(__dirname, '..', basePath, ('/' + o.module)))
+      o.vueFile.forEach(v => {
+        createVueFileFromOrigin('/' + o.module + '/' + v.name)
+        console.log('create ' + v.name + ' success')
+      })
+    }
+  })
+},
+initRouter: para => {
+  if (fs.existsSync(routerFile)) {
+    let bakFile = routerFile.replace(/index\.js/, 'index_bak.js')
+    fs.renameSync(routerFile, bakFile)
+  }
+  findFile(basePath).then(() => {
+    let data = ''
+    data += "import Vue from 'vue'\r\n"
+    data += "import Router from 'vue-router'\r\n"
+    data += "//import pages below\r\n"
+    let routerList = []
+    vueFileList.forEach((o, i) => {
+      data += "import " + o.name + " from '" + o.path + "'\r\n"
+      if (o.name === 'dev') {
+        routerList.push((i === 0 ? '' : ' ') + "{\r\n\t\tpath: '/',\r\n\t\tname: '" + o.name + "',\r\n\t\tcomponent: " + o.name + "\r\n\t}")
+      } else {
+        routerList.push((i === 0 ? '' : ' ') + "{\r\n\t\tpath: '" + o.link + "',\r\n\t\tname: '" + o.name + "',\r\n\t\tcomponent: " + o.name + "\r\n\t}")
+      }
+    })
+    data += "\nVue.use(Router)\r\n\n"
+    data += "export default new Router({\r\n\troutes: [" + routerList + "]\r\n})\r\n"
+    fs.writeFile(routerFile, data, err => {
+      if (err) {
+        console.error(err)
+      } else {
+        console.log('vue-router file created success!')
+      }
+    })
+  })
+},
+initApi: para => {
+  let apiFileAppend = '// 引入API模块\r\n'
+  let apiFileAppend2 = 'Object.assign(moduleAPI'
+  para.forEach(o => {
+    if (!fs.existsSync(`./src/api/${o}.js`)) {
+      createApiModule(o)
+      apiFileAppend += `import * as ${o}API from './${o}'\r\n`
+      apiFileAppend2 += `, ${o}API`
+    }
+  })
+  fs.writeFileSync('./src/api/index.js', fs.readFileSync('./src/api/index.js', 'utf-8').replace(/\/\/ 引入API模块/, apiFileAppend).replace(/Object\.assign\(moduleAPI/, apiFileAppend2))
+},
+initVuex: para => {
+  let storeFileAppend = '// 引入vuex模块\r\n'
+  let storeFileAppend2 = 'modules: {\r\n'
+  para.forEach(o => {
+    if (!fs.existsSync(`./src/store/modules/${o}.js`)) {
+      createStoreModule(o)
+      storeFileAppend += `import ${o}Module from './modules/${o}'\r\n`
+      storeFileAppend2 += `\t\t${o}Module,\r\n`
+    }
+  })
+  fs.writeFileSync('./src/store/index.js', fs.readFileSync('./src/store/index.js', 'utf-8').replace(/\/\/ 引入vuex模块/, storeFileAppend).replace(/modules: \{/, storeFileAppend2))
+}
 }
